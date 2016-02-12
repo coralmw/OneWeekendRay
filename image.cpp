fd773462cabf1ee797c35f7599ea15067974fcdf
Thomas Parks <thomasparks@outlook.com>
2016-02-12 22:51:11 +0000

    getting more normal..... s on the spheres

--- image.cpp
+++ image.cpp
@@ -2,9 +2,27 @@
 #include "ray.h"
 #include "vec3.h" /* inc. by ray as well, but written here for clarity */
 
+float hit_sphere(const vec3& center, float radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0 * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius*radius;
+  float discrim =  b*b - 4*a*c;
+  if (discrim < 0) {
+    return -1.0;
+  } else {
+    return (-b - sqrt(discrim)) / (2.0*a);
+  }
+}
+
 vec3 color(const ray& r) {
+  float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+  if (t > 0.0){
+    vec3 N = unit_vector(r.point_at(t) - vec3(0,0,-1));
+    return 0.5*vec3(N.x()+1,N.y()+1, N.z()+1);
+  }
   vec3 unit_direction = unit_vector(r.direction());
-  float t = 0.5*(unit_direction.y() + 1.0);
+  t = 0.5*(unit_direction.y() + 1.0);
   return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
 }
 
@@ -25,7 +43,7 @@ int main() {
 
       ray r(origin, lower_left_corner + u*horizontal + v*vertical);
       vec3 col = color(r);
-      
+
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
