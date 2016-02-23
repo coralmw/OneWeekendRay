77aaaacb438ecf12e70d969f161ad6eac14ae675
Thomas Parks <thomasparks@outlook.com>
2016-02-23 17:45:51 +0000

    Adding some kind of user interface.

--- image.cpp
+++ image.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <fstream>      // std::ofstream
+
+#include <string>
+#include <stdio.h>
+
 #include "camera.h"
 #include "material.h"
 #include "sphere.h"
@@ -39,15 +44,47 @@ vec3 color(const ray& r, hitable *world, int depth) {
 //   list[1] = new sphere(vec3(0,-100.5,-1), 100, new metal(vec3(0.8, 0.7, 0.3), 0.5));
 // }
 
-void fill(hitable *l[]){
-  l[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+int fill(hitable *l[]){
+  // format
+
+  // NUM OBJS
+  // OBJECT TYPE
+  // OBJECT SPEC
+  // rep
+  char *curr_line;
+  int bytes_read;
+  size_t nbytes = 1000;
+  curr_line = (char *)malloc(nbytes + 1);
+  bytes_read = getline(&curr_line, &nbytes, stdin);
+
+  int num_objs;
+  sscanf(curr_line, "%i", &num_objs);
+
+  for (int i = 0; i < num_objs; i++){
+    std::cout << "line no: " << i << "\n";
+    getline(&curr_line, &nbytes, stdin);
+    if (strncmp("sphere", curr_line, 6) == 0){
+      getline(&curr_line, &nbytes, stdin);
+      if (strncmp("glass", curr_line, 5)  == 0) {
+        float x, y, z;
+        float lx, ly, lz;
+        float size;
+        getline(&curr_line, &nbytes, stdin);
+        sscanf(curr_line, "%f %f %f %f %f %f %f", &x, &y, &z, &size, &lx, &ly, &lz);
+        l[i] = new sphere(vec3(x,y,z), size, new lambertian(vec3(lx,ly,lz)));
+      }
+    }
+
+  }
+  return num_objs;
 }
 
-int main() {
-  int nx = 200;
-  int ny = 100;
-  int ns = 10;
-  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+int main(int argc, char* argv[]) {
+  std::ofstream outfile(argv[1], std::ofstream::out);
+  int nx = 600;
+  int ny = 300;
+  int ns = 20;
+  outfile << "P3\n" << nx << " " << ny << "\n255\n";
 
   vec3 lower_left_corner(-2.0, -1.0, -1.0);
   vec3 horizontal(4.0, 0.0, 0.0);
@@ -55,9 +92,9 @@ int main() {
   vec3 origin(0.0, 0.0, 0.0);
 
   hitable *list[1000];
-  fill(list);
+  int num_objs = fill(list);
 
-  hitable *world = new hitable_list(list,1);
+  hitable *world = new hitable_list(list,num_objs);
   camera cam(vec3(0,0,-4), vec3(0,0,-1), vec3(0,1,0), 45, float(nx)/float(ny));
 
   for (int j = ny-1; j >= 0; j--){
@@ -76,7 +113,8 @@ int main() {
       int ir = int(255.99*col[0]);
       int ig = int(255.99*col[1]);
       int ib = int(255.99*col[2]);
-      std::cout << ir << " " << ig << " " << ib << "\n";
+      outfile << ir << " " << ig << " " << ib << "\n";
     }
   }
+  outfile.close();
 }
